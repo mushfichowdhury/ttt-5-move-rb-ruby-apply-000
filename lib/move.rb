447bd80ae2f492cb3322adcb75dd82b02d27cbf5
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-10 16:17:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,9 +9,9 @@ def display_board(board)
   
 end
 
-#def input_to_index(user_input)
- #user_input = user_input.to_i - 1
-#end
+def input_to_index(user_input)
+ user_input = user_input.to_i - 1
+end
 
 def move(board, user_input, char = "X")
   user_input = user_input.to_i - 1
